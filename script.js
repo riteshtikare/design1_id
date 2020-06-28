7acf5ee70e7e6c896221af19f5dce0c730ec32e4
riteshtikare <60392084+riteshtikare@users.noreply.github.com>
2020-06-28 21:24:46 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+gsap.from('#nav h3',{duration: 1, ease:'expo.inOut' , x: 40, opacity: 0});
+
+
+gsap.from('#links a',{stagger: .2 ,duration: 1, ease:'expo.inOut' , x: 40, opacity: 0});
+
+gsap.from('#rightopts .ranim',{delay: 1 ,stagger:.2, duration:1, ease:'expo.inOut', x: 30 ,opacity:0})
+
+gsap.from('#right',{duration: 2.5, ease:'expo.inOut' , x: '100%', opacity: 0});
+
+gsap.from('#heading .manim',{stagger:.3,duration: 2.5, ease:'expo.inOut' , y: '55%', opacity: 0});
+
+gsap.from('#heading #line ',{delay: 1.5, duration: 1, ease:'expo.inOut' , width: '0%', opacity: 0});
+
+gsap.from('#credits img',{stagger:.3,duration: 1, ease:'expo.inOut' , y: '70%', opacity: 0});
+
+gsap.from('#right img',{delay:1.7 ,duration: 1.5, ease:'expo.inOut' , scale:1});
+
+
+
+
